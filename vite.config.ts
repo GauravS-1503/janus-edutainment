ffc9144300078403066ae74309f82a9036b1afaf
GauravS-1503 <gauravsriram751@gmail.com>
2026-02-01 19:05:45 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/janus-edutainment/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
